3b7991a121f4df69d8b7455916b4837cb73fa295
Kim Aleksandr <a.kim@jelata.tech>
2023-05-30 18:01:51 +0700

    начал hero компонент

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,22 @@
+import './Hero.css';
+
+const Hero = () => {
+   return (
+    <section className='hero'>
+      <h1>Учебный проект студента факультета Веб-разработки.</h1>
+      <ul>
+        <li>
+          <a href="#"></a>
+        </li>
+        <li>
+          <a href="#"></a>
+        </li>
+        <li>
+          <a href="#"></a>
+        </li>
+      </ul>
+    </section>
+  )
+}
+
+export default Hero;
