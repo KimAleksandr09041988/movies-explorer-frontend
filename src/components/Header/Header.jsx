ac6d79e3c9762f091733f726dd84e40465e2d71b
Kim Aleksandr <a.kim@jelata.tech>
2023-05-30 16:39:06 +0700

    header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,20 +5,21 @@ import logo from '../../images/logo.svg'
 import './Header.css'
 
 const Header = () => {
-  const width = window.screen.width;
-  console.log(width);
-    return (
-        <header className='header'>
-            <Link to='/' className='header__link_logo'>
-                <img src={logo} className='header__logo' alt='Логотип' />
-            </Link>
-            <Navigation />
-            <button type='button' className='header__btn-burger'>
-              кнопка
-              <span className=''></span>
-            </button>
-        </header>
-    )
+  const [loggin, setLoggin] = React.useState(true);
+
+  return (
+      <header className='header'>
+          <Link to='/' className='header__link_logo'>
+              <img src={logo} className='header__logo' alt='Логотип' />
+          </Link>
+          <Navigation />
+          {loggin &&
+          <button type='button' className='header__btn-burger'>
+            кнопка
+            <span className=''></span>
+          </button>}
+      </header>
+  )
 }
 
-export default Header;  
+export default Header;
--- src/components/Header/components/Navigation/Navigation.jsx
+++ src/components/Header/components/Navigation/Navigation.jsx
@@ -1,21 +1,25 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-
-
+import './Navigation.css';
 
 const Navigation = () => {
-    const [loggin, setLoggin] = React.useState(false);
+    const [loggin, setLoggin] = React.useState(true);
 
-    
     return (
         <nav className='navigation'>
-            <ul>
-                <li>
-                    <NavLink to={!loggin ? '/signup' : '/movies'}>{!loggin ? 'Регистарация' : "Фильмы"}</NavLink>
-                    <NavLink to={!loggin ? '/signin' : '/savemovies'}>{!loggin ? 'Войти' : "Сохранённые фильмы"}</NavLink>
+            <ul className='navigation__list'>
+                <li className='navigation__item'>
+                    <NavLink className='navigation__link' to={!loggin ? '/signup' : '/movies'}>{!loggin ? 'Регистарация' : "Фильмы"}</NavLink>
                 </li>
+                <li className='navigation__item'>
+                    <NavLink to={!loggin ? '/signin' : '/savemovies'} className={loggin ? 'navigation__link' : 'navigation__link navigation__link_enter'}>{!loggin ? 'Войти' : "Сохранённые фильмы"}</NavLink>
+                </li>
+                {loggin &&
+                  <li className='navigation__item'>
+                    <NavLink className='navigation__link navigation__link_profile' to='/profile' style={{ marginLeft: '30px' }}>Аккаунт</NavLink>
+                  </li>
+                }
             </ul>
-            {loggin ? <NavLink to='/profile'>Аккаунт</NavLink> : ""}
         </nav>
     )
 }
