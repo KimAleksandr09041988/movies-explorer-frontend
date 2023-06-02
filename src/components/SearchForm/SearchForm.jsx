4736b9a899da6675935fad9e5a535833b9c6efcf
Kim Aleksandr <a.kim@jelata.tech>
2023-06-02 17:56:51 +0700

    фикс

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -5,20 +5,22 @@ import React from 'react';
 const SearchForm = () => {
   return (
     <section className='search-form'>
-      <h1 className='search-form__header'></h1>
-      <form className='search-form__form'>
-        <fieldset>
-          <input className='search-form__input' name='search' type="text" aria-label='Поиск' placeholder='Поиск' />
-          <button type='submit'></button>
-          <label className='search-form__label' >
-            <input type="checkbox" className='search-form__checkbox' />
-            <span className='search-form__wrapper-custom-checkbox'>
-              <span className='search-form__circle-custom-checkbox'></span>
-            </span>
-            <p className='search-form__label-description'>Короткометражки</p>
-          </label>
-        </fieldset>
-      </form>
+      <h1 className='search-form__header'>Фильмы</h1>
+      <div className='container'>
+        <form className='search-form__form'>
+          <fieldset className='search-form__fieldset'>
+            <input className='search-form__input' name='search' type="text" aria-label='Поиск' placeholder='Поиск' />
+            <button type='submit'></button>
+            <label className='search-form__label' >
+              <input type="checkbox" className='search-form__checkbox' />
+              <span className='search-form__wrapper-custom-checkbox'>
+                <span className='search-form__circle-custom-checkbox'></span>
+              </span>
+              <p className='search-form__label-description'>Короткометражки</p>
+            </label>
+          </fieldset>
+        </form>
+      </div>
     </section>
   )
 }
