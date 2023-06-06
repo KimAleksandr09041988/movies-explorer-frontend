cc7b412ab5813eafb64d9be0cc2c1e92ccac3792
Kim Aleksandr <a.kim@jelata.tech>
2023-06-06 14:41:02 +0700

    fix navigation header promo

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,43 +1,79 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 import './Navigation.css';
 
 const Navigation = () => {
-  const [loggin, setLoggin] = React.useState(true);
+  const [loggin, setLoggin] = React.useState(false);
   return (
-        <nav className={!loggin ? 'navigation' : 'navigatin navigation_loggin'}>
+    <>
+      { loggin ? (
+        <nav>
           <ul className='navigation__list'>
-              <li className='navigation__item'>
-                <NavLink
-                  className='navigation__link'
-                  to={!loggin ? '/signup' : '/movies'}
-                  activeClassName='navigation__link navigation__link_active'
-                  >
-                    {!loggin ? 'Регистарация' : "Фильмы"}
-                </NavLink>
-              </li>
-              <li className='navigation__item'>
-                  <NavLink
-                    to={!loggin ? '/signin' : '/saved-movies'}
-                    className={loggin ? 'navigation__link' : 'navigation__link navigation__link_enter'}
-                    >
-                      {!loggin ? 'Войти' : "Сохранённые фильмы"}
-                  </NavLink>
-              </li>
-              {loggin &&
-                <li className='navigation__item'>
-                  <NavLink
-                    className='navigation__link navigation__link_profile'
-                    to='/profile'
-                    style={{ marginLeft: '30px' }}
-                    >
-                      Аккаунт
-                  </NavLink>
-                </li>
+            <li className='navigation__item_type_signin'>
+              <NavLink
+                className={({isActive}) =>
+                  isActive ?
+                    'navigation__link navigation__link_type_signin navigation__link_active' :
+                    'navigation__link navigation__link_type_signin'
               }
+                to='/movies'
+              >
+                Фильмы
+              </NavLink>
+            </li>
+            <li className='navigation__item_type_signin'>
+              <NavLink
+                className={({isActive}) =>
+                  isActive ?
+                    'navigation__link navigation__link_type_signin navigation__link_active' :
+                    'navigation__link navigation__link_type_signin'
+                }
+                to='/saved-movies'
+              >
+                Сохранённые фильмы
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                className={({isActive}) =>
+                  isActive ?
+                  'navigation__link navigation__link_type_signin navigation__link_profile navigation__link_active' :
+                  'navigation__link navigation__link_type_signin navigation__link_profile'
+                }
+                to='/profile'
+              >
+                Аккаунт
+              </NavLink>
+            </li>
           </ul>
-      </nav>
+        </nav>
+      ) : (
+        <nav>
+          <ul className='navigation__list'>
+            <li className='navigation__item_type_logout'>
+              <Link
+                className='navigation__link_type_logout
+                           navigation__link'
+                to='/signup'
+              >
+                Регистрация
+              </Link>
+            </li>
+            <li className='navigation__item_type_logout'>
+              <Link
+                className='navigation__link_type_logout                   navigation__link_enter
+                           navigation__link'
+                to='/signin'
+              >
+                Войти
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      )}
+    </>
   )
 }
 
 export default Navigation;
+
