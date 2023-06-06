6ca7e77d1eb9976ea40f0c3f51d42356431772a4
Kim Aleksandr <a.kim@jelata.tech>
2023-06-06 12:33:34 +0700

    fix

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -8,14 +8,31 @@ const Navigation = () => {
         <nav className={!loggin ? 'navigation' : 'navigatin navigation_loggin'}>
           <ul className='navigation__list'>
               <li className='navigation__item'>
-                  <NavLink className='navigation__link' to={!loggin ? '/signup' : '/movies'}>{!loggin ? 'Регистарация' : "Фильмы"}</NavLink>
+                <NavLink
+                  className='navigation__link'
+                  to={!loggin ? '/signup' : '/movies'}
+                  activeClassName='navigation__link navigation__link_active'
+                  >
+                    {!loggin ? 'Регистарация' : "Фильмы"}
+                </NavLink>
               </li>
               <li className='navigation__item'>
-                  <NavLink to={!loggin ? '/signin' : '/saved-movies'} className={loggin ? 'navigation__link' : 'navigation__link navigation__link_enter'}>{!loggin ? 'Войти' : "Сохранённые фильмы"}</NavLink>
+                  <NavLink
+                    to={!loggin ? '/signin' : '/saved-movies'}
+                    className={loggin ? 'navigation__link' : 'navigation__link navigation__link_enter'}
+                    >
+                      {!loggin ? 'Войти' : "Сохранённые фильмы"}
+                  </NavLink>
               </li>
               {loggin &&
                 <li className='navigation__item'>
-                  <NavLink className='navigation__link navigation__link_profile' to='/profile' style={{ marginLeft: '30px' }}>Аккаунт</NavLink>
+                  <NavLink
+                    className='navigation__link navigation__link_profile'
+                    to='/profile'
+                    style={{ marginLeft: '30px' }}
+                    >
+                      Аккаунт
+                  </NavLink>
                 </li>
               }
           </ul>
