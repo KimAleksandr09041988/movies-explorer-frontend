a9718c5843e486bd7eac21834b09f844543d59ec
Kim Aleksandr <a.kim@jelata.tech>
2023-06-15 12:24:05 +0700

    fix

--- src/utils/Auth.js
+++ src/utils/Auth.js
@@ -1,5 +1,5 @@
 export const registration = async (email, password, name) => {
-  const res = await fetch('http://127.0.0.1:3001/signup', {
+  const res = await fetch('http://api.movies.kim.nomoredomains.rocks/signup', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
